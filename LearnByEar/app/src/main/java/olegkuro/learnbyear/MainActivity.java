8b8b7a08e498f246dd59fa8c0ed684befb27ce76
romagolchin <gol4in.roman@yandex.ru>
2016-11-29 20:01:59 +0300

    added Firebase dependencies

--- LearnByEar/app/src/main/java/olegkuro/learnbyear/MainActivity.java
+++ LearnByEar/app/src/main/java/olegkuro/learnbyear/MainActivity.java
@@ -1,8 +1,10 @@
 package olegkuro.learnbyear;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Елена on 29.11.2016.
  */
 
-public class MainActivity {
+public class MainActivity extends AppCompatActivity {
 }
