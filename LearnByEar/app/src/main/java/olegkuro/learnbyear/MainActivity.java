42e5ec9bbfb80da8982aa883823cb0887a3b2a14
OlegKuro <hayatov@rain.ifmo.ru>
2016-12-07 01:16:51 +0500

    menu created... TODO: handle

--- LearnByEar/app/src/main/java/olegkuro/learnbyear/MainActivity.java
+++ LearnByEar/app/src/main/java/olegkuro/learnbyear/MainActivity.java
@@ -4,6 +4,8 @@ package olegkuro.learnbyear;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /**
  * Created by Елена on 29.11.2016.
@@ -20,4 +22,34 @@ public class MainActivity extends Activity {
         //outState.putAll();
         //TODO put identifiers here
     }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        // adds menu items to the action bar if it presents
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //TODO menu event handlers
+        //Change Language
+        if (id == R.id.action_item1){
+            return true;
+        }
+
+        //show Authors
+        if (id == R.id.action_item2){
+            return true;
+        }
+
+        //Exit
+        if (id == R.id.action_item3){
+            return true;
+        }
+
+        //delete it later pls
+        return true;
+    }
+
 }
