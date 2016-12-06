1bfb390c41e0167927a8dff86677f80e2d853e52
OlegKuro <hayatov@rain.ifmo.ru>
2016-12-07 00:30:21 +0500

    test

--- LearnByEar/app/src/main/java/olegkuro/learnbyear/MainActivity.java
+++ LearnByEar/app/src/main/java/olegkuro/learnbyear/MainActivity.java
@@ -1,10 +1,23 @@
 package olegkuro.learnbyear;
 
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by Елена on 29.11.2016.
  */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        //outState.putAll();
+        //TODO put identifiers here
+    }
 }
