dabe1f72440330851d915e684fa7e3c054a3b700
romagolchin <gol4in.roman@yandex.ru>
2016-12-07 13:01:07 +0300

    started working on authentication

--- LearnByEar/app/src/main/java/olegkuro/learnbyear/MainActivity.java
+++ LearnByEar/app/src/main/java/olegkuro/learnbyear/MainActivity.java
@@ -1,10 +1,22 @@
 package olegkuro.learnbyear;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by Елена on 29.11.2016.
  */
 
 public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
+
+    }
 }
